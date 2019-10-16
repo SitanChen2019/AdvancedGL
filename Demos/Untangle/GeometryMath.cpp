ff2878a242bcce830f2c3acf252446c142d33a24
SitanChen <2676628214@qq.com>
2019-10-16 18:03:55 +0800

    implement triangle triangle intersection

--- Demos/Untangle/GeometryMath.cpp
+++ Demos/Untangle/GeometryMath.cpp
@@ -187,9 +187,190 @@ namespace GeometryMath {
 
 	bool Triangle::intersect_traingle(const Triangle& tri) const
 	{
-		return getBoundingBox().intersect_AABB(tri.getBoundingBox());
+		//return getBoundingBox().intersect_AABB(tri.getBoundingBox());
 
+		//a faster triangle-to-triangle intersection test algorithm.pdf
+
+		//stage 1
+		glm::vec3 r0 = tri.m_v0 - m_v2;
+		glm::vec3 r1 = tri.m_v1 - m_v2;
+		glm::vec3 r2 = tri.m_v2 - m_v2;
+
+
+		glm::vec3 e0 = tri.m_v0 - tri.m_v2;
+		glm::vec3 e1 = tri.m_v1 - tri.m_v2;
+
+		glm::vec3 n = glm::cross(e0, e1);
+		float D0 = glm::dot(r0, n);
+		float D1 = glm::dot(r1, n);
+		float D2 = glm::dot(r2, n);
+
+		int positive_num = 0;
+		int negitive_num = 0;
+		int near_zero_num = 0;
+
+		int sign_key = 0;
+		int near_zero_value = 0;
+		for (float value : std::array<float, 3>{D0,D1,D2})
+		{
+			if (value > 1e-5)
+			{
+				sign_key = (sign_key << 1) + 1;
+
+			}
+			else if (value < -1e-5)
+			{
+				sign_key = (sign_key << 1) + 0;
+			}
+			else
+			{
+				++near_zero_num;
+			}
+		}
+
+		glm::vec3 t0, t1;
+		if (near_zero_num > 0)
+		{
+			//TODO
+			throw std::exception();
+		}
+		else
+		{
+			switch (sign_key)
+			{
+				case 0:  //---
+				case 7:  //+++
+					return false;
+				case 1:  //++-
+				case 6:  //--+
+				{
+					//02,12
+					float beta02 = D0 / (D0 - D2);
+					float beta12 = D1 / (D1 - D2);
+					t0 = beta02 * r0 + (1 - beta02) * r2;
+					t1 = beta12 * r1 + (1 - beta12) * r2;
+				}
+					break;
+				case 2:  //-+-
+				case 5:  //+-+
+				{
+					//12,01
+					float beta12 = D1 / (D1 - D2);
+					float beta01 = D0 / (D0 - D1);
+					t0 = beta12 * r1 + (1 - beta12) * r2;
+					t1 = beta01 * r0 + (1 - beta01) * r1;
+				}
+					break;
+				case 3: //-++
+				case 4: //+--
+				{
+					//01,02
+					float beta01 = D0 / (D0 - D1);
+					float beta02 = D0 / (D0 - D2);
+					t0 = beta01 * r0 + (1 - beta01) * r1;
+					t1 = beta02 * r0 + (1 - beta02) * r2;
+				}
+					break;
+			default:
+				break;
+			}
+		}
+
+		int stage2_key = 0;
+		glm::vec3 e0t0 = glm::cross(e0, t0);
+		glm::vec3 e1t0 = glm::cross(e1, t0);
+		glm::vec3 e0t1 = glm::cross(e0, t1);
+		glm::vec3 e1t1 = glm::cross(e1, t1);
+		
+		for (auto& vect : std::array<glm::vec3, 4>{e0t0, e1t0, e0t1, e1t1})
+		{
+			stage2_key = (stage2_key << 1) + glm::dot(vect, n) > 0 ? 1 : 0;
+		}
+
+		int  result_matrix[16] = {
+			-1,-1,1,2,
+			-1,-1,4,-1,
+			1,4,3,1,
+			2,-1,1,-1
+		};
+		int stage2_case = result_matrix[stage2_key];
+		
+		if (stage2_case == 1)
+		{
+			glm::vec3 p_in = t0;
+			glm::vec3 p_out = t1;
+			if (stage2_key == 2 || stage2_key == 14)
+			{
+				p_in = t1;
+				p_out = t0;
+			}
+
+			glm::vec3 feature_edge = e0;
+			float signal = -1.0f;
+			if (stage2_key == 11 || stage2_key == 14)
+			{
+				feature_edge = e1;
+				signal = 1.0f;
+			}
+
+
+			//check p_in if in the triangle, if yes,must intersection
+			//(p_in - e0)x(p_in - e1) equidirectional to n
+			if (glm::dot(glm::cross(p_in - e0, p_in - e1), n) >= 0)
+			{
+				return true;
+			}
+			else if( glm::dot( glm::cross(p_out - feature_edge, p_in -feature_edge) , signal*n ) >= 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else if (stage2_case == 2)
+		{
+			glm::vec3 p_3 = t0;
+			glm::vec3 p_1 = t1;
+
+			if (stage2_key == 12)
+			{
+				p_1 = t1;
+				p_3 = t0;
+			}
+
+			if (glm::dot(glm::cross(p_3, p_1), n) >= 0)
+			{
+				if (glm::dot(glm::cross(p_1 - e0, p_3 - e0), n) >= 0)
+				{
+					return true;
+				}
+				else if (glm::dot(glm::cross(p_1 - e1, p_3 - e1), n) >= 0)
+				{
+					return true;
+				}
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else if (stage2_case == 3)
+		{
+
+		}
+		else if (stage2_case == 4)
+		{
+
+		}
+		else
+		{
+			assert(false);
+		}
 	}
+
+
 
 	AABB Triangle::getBoundingBox() const
 	{
@@ -215,6 +396,7 @@ namespace GeometryMath {
         Triangle t(p10, p11, p12);
         return  t.intersect_segment(p00, p01, hit_t);;
     }
+
 
 }
 
